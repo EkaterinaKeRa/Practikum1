b86cf51ace4142e1c54c74fbd4d38dd5a3de2cae
EkaterinaKeRa <ekaterinaoncovet@gmail.com>
2023-03-27 22:11:32 +0300

    Initial Commit

---
+++ Practikum1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practikum1
+//
+//  Created by Екатерина on 27.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
